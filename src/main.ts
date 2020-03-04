a9faf1c7c14e2732defaad232cf682b641b8d80c
Sebastian Brown <SBrown@forsythesit.com.au>
2020-03-04 12:12:54 +1100

    Intial commit

---
+++ src/main.ts
@@ -0,0 +1,71 @@
+import * as artifact from "@actions/artifact";
+import * as core from "@actions/core";
+import { exec } from "@actions/exec";
+import * as github from "@actions/github";
+
+async function main() {
+	const octopusUrl: string = core.getInput("OCTOPUS_URL", { required: true });
+	const octopusApiKey: string = core.getInput("OCTOPUS_APIKEY", { required: true });
+	const solutionPath: string = core.getInput("SOLUTION_PATH", { required: true });
+	const project: string = core.getInput("PROJECT", { required: false });
+	const deployTo: string = core.getInput("DEPLOY_TO", { required: false });
+	const msTeamsWebhook: string = core.getInput("MS_TEAMS_WEBHOOK", { required: false });
+	const context = github.context;
+	const repo = context.repo.repo;
+	const projectName = project ? project : repo;
+	try {
+
+		if (context.ref.indexOf("refs/tags/") === -1) {
+			throw new Error("Unable to get a version number");
+		}
+		const version = context.ref.replace("refs/tags/", "");
+		core.info(`🐙 Deploying project ${projectName} (Version ${version}) to Octopus `);
+		core.info("Installing octopus cli...");
+		await exec(`dotnet tool install octopus.dotnet.cli --tool-path .`);
+		core.info("Packing Solution...");
+		await exec(`.\\dotnet-octo.exe pack --id=${repo} --outFolder=output --basePath=${solutionPath} --version=${version}`);
+		core.info("Pushing to Octopus...");
+		await exec(`.\\dotnet-octo push --package=output\\${repo}.${version}.nupkg --server=${octopusUrl} --apiKey=${octopusApiKey}`);
+		core.info("Creating Release...");
+		const deployToString = deployTo ? `--deployTo=${deployTo}` : "";
+		await exec(`.\\dotnet-octo create-release --project=${projectName} --version=${version} --server=${octopusUrl} --apiKey=${octopusApiKey} ${deployToString}`);
+		if (msTeamsWebhook) {
+			sendTeamsNotification(projectName, `✔ Version ${version} Deployed to Octopus`, msTeamsWebhook);
+		}
+		core.info("✅ complete");
+	} catch (err) {
+		core.error("❌ Failed");
+		if (msTeamsWebhook) {
+			sendTeamsNotification(projectName, `❌ Failed to deploy to Octopus (ref: ${context.ref})`, msTeamsWebhook);
+		}
+		core.setFailed(err.message);
+	}
+}
+
+async function downloadArtifact(artifactName: string) {
+	core.info(`Downloading package artifact ${artifactName}...`);
+	const artifactClient = artifact.create();
+	try {
+		const response = await artifactClient.downloadArtifact(artifactName);
+		core.info(`Downloaded ${response.artifactName}`);
+	} catch (error) {
+		core.error("❌ Could not retrieve artifact");
+		core.setFailed(error.message);
+
+	}
+	await exec(`ls`);
+	await exec(`pwd`);
+}
+
+/**
+ * Sends a MS Teams notification
+ * @param title
+ * @param body
+ */
+async function sendTeamsNotification(title: string, body: string, webhookUrl: string) {
+	const data = `"{ '@context': 'http://schema.org/extensions', '@type': 'MessageCard', 'title': '${title}', 'text': '${body}' }"`;
+	core.info("Sending Teams notification...");
+	await exec(`curl --url "${webhookUrl}" -d ${data}`);
+}
+
+main();
